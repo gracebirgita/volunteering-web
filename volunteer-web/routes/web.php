9633abbae0401779909810dc67e084b0e2c0e684
Morcime <calistanurcahya@gmail.com>
2025-12-26 16:58:06 +0700

    feat(7) routes to setting protected by login

--- volunteer-web/routes/web.php
+++ volunteer-web/routes/web.php
@@ -12,16 +12,27 @@ Route::get('/', function () {
     return view('landingPage');
 });
 
+Route::middleware(['auth'])->group(function(){
+    Route::get('/settings', [VolunteerSettingsController::class, 'edit'])->name('volunteer.settings');
+
+    Route::post('/settings/profile', [VolunteerSettingsController::class, 'updateProfile'])
+        ->name('volunteer.settings.profile');
+
+    Route::post('/settings/password', [VolunteerSettingsController::class, 'updatePassword'])
+        ->name('volunteer.settings.password');
+
+});
+
 
 //login routes blm, harus login dulu
-Route::get('/settings', [VolunteerSettingsController::class, 'edit'])
-    ->name('volunteer.settings');
+// Route::get('/settings', [VolunteerSettingsController::class, 'edit'])
+//     ->name('volunteer.settings');
 
-Route::post('/settings/profile', [VolunteerSettingsController::class, 'updateProfile'])
-    ->name('volunteer.settings.profile');
+// Route::post('/settings/profile', [VolunteerSettingsController::class, 'updateProfile'])
+//     ->name('volunteer.settings.profile');
 
-Route::post('/settings/password', [VolunteerSettingsController::class, 'updatePassword'])
-    ->name('volunteer.settings.password');
+// Route::post('/settings/password', [VolunteerSettingsController::class, 'updatePassword'])
+//     ->name('volunteer.settings.password');
 
 
 require __DIR__.'/auth.php';
